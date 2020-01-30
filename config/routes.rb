878472e44f458b5f5ad1dc0964b606d35b263bc2
estevamponte <tecoponte@gmail.com>
2020-01-30 08:36:34 -0300

    Configurando arquivo de rotas para api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+    
+  end
 end
+
+
+# namespace :api => Cria um caminho para acessar aque controller ex: www.localhost:3000/api
+# defaults: { format: :json } => Diz que o formato da saida é json
+# constraints: { subdomain: 'api' } => Diz que so pode entrar no bloco se tiver um sub dominio 'api'
+# path: "/" => Troca o 'www.localhost:3000/api' para somente www.localhost:3000/ que é o root
