ff0b1bcaecc2042c02415fb616830504eb093f8f
estevamponte <tecoponte@gmail.com>
2020-01-31 21:17:35 -0300

    Adicionando a autenticacao token ao user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,30 @@ RSpec.describe User, type: :model do
 
   describe '#info' do
     it 'returns email, created_at and a Token' do
-      user.save!
+      user.save! # Com o '!' o user.save ira tentar salvar, caso não consiga salvar é gerado uma exeção, sem o '!' a exeção não é gerada
       allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
 
       expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123xyzTOKEN")
     end
   end
+
+  describe '#generate_authentication_token!' do # O '!' na frente do metodo significa que algum atributo sera alterado, que ira gerar uma mudança no estado do atributo 'auth_token'
+    it 'generates a unique auth token' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+      user.generate_authentication_token!
+
+      expect(user.auth_token).to eq('abc123xyzTOKEN')
+    end
+
+    it 'generate another auth token when the current auth token already has been take' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abc123tokenxyz','abcXYZ123456789')
+      existing_user = create(:user)
+      user.generate_authentication_token!
+
+      expect(user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end
+
 
   # context 'when name is blank' do
   #   before(:each) { user.name = ' ' }
