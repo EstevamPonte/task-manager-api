62de63e11147cf995921bd1be8cb63596b68c23f
estevamponte <tecoponte@gmail.com>
2020-02-02 16:00:17 -0300

    Adicionando a action destroy ao controller task com teste

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy] # Esse é um formato de um CRUD 'show' = GET, 'create' = POST, 'update' = PUT, 'destroy' = DELETE
       resources :sessions, only: [:create, :destroy]
-      resources :tasks, only: [:index, :show, :create, :update] # 'index' siginifica um array contendo varias tarefas
+      resources :tasks, only: [:index, :show, :create, :update, :destroy] # 'index' siginifica um array contendo varias tarefas
     end
   end
 end
