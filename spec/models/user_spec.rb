9eb1ebc52fac6e57fc005ffb8457cc562fdba953
estevamponte <tecoponte@gmail.com>
2020-02-04 07:57:06 -0300

    Instalando o devise_token_auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_many(:tasks).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive } # verifica se a emails iguais
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive.scoped_to(:provider) } # verifica se a emails iguais
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value('costa@nonato.com').for(:email)}
   it { is_expected.to validate_uniqueness_of(:auth_token) }
